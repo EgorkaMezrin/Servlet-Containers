4226a6ca1a4f65c346ecc121243de224361f6f36
satan <e-mezrin2012@yandex.ru>
2024-03-16 11:46:25 +0400

    di java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,67 +1,72 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private static PostController controller;
-  private static final String GET = "GET";
-  private static final String POST = "POST";
-  private static final String DELETE = "DELETE";
-  private static final String API_POSTS = "/api/posts";
-  private static final String API_POSTS_D = "/api/posts/\\d+";
 
+    private final String GET_METHOD = "GET";
+    private final String POST_METHOD = "POST";
+    private final String DELETE_METHOD = "DELETE";
+    private final String PATH_FOR_READ_ALL_POSTS = "/api/posts";
+    private final String PATH_FOR_SAVE_POST = "/api/posts";
+    private final String REGEX_FOR_GET_AND_REMOVE_POST = "/api/posts/\\d+";
+    private final String SLASH = "/";
+    private PostController controller;
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
+    @Override
+    public void init() {
+        // Specify package list for classes with annotations
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
 
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-
-      if (path.matches(API_POSTS_D)) {
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-
-        if (method.equals(GET)) {
-          // easy way
-          controller.getById(id, resp);
-          return;
-        }
-
-        if (method.equals(DELETE)) {
-          // easy way
-          controller.removeById(id, resp);
-          return;
-        }
-      }
-      // primitive routing
-      if (method.equals(GET) && path.equals(API_POSTS)) {
-        controller.all(resp);
-        return;
-      }
-
-      if (method.equals(POST) && path.equals(API_POSTS)) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        // Get controller by bean's class
+        controller = context.getBean(PostController.class);
     }
-  }
-}
 
+    private long getId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf(SLASH) + 1));
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // it is enough if project is deployed to root (/) context
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            final long id;
+
+            System.out.println("path = " + path);
+            System.out.println("method = " + method);
+            // primitive routing
+            if (method.equals(GET_METHOD) && path.equals(PATH_FOR_READ_ALL_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET_METHOD) && path.matches(REGEX_FOR_GET_AND_REMOVE_POST)) {
+                // easy way
+                id = getId(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(POST_METHOD) && path.equals(PATH_FOR_SAVE_POST)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE_METHOD) && path.matches(REGEX_FOR_GET_AND_REMOVE_POST)) {
+                // easy way
+                id = getId(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
